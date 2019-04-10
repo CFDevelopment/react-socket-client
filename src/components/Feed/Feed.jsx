7d0b2643b76a9c5bc3239dd0f9e55dae87a72f6e
Chris Fenos <chris.m.fenos@gmail.com>
2019-04-10 17:09:28 -0400

    custom message display

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,19 +1,20 @@
 import React, { Component } from "react";
 import "./Feed.scss";
+import Message from '../Message/index';
 
 class Feed extends Component {
-  render() {
-    const info = this.props.feed.map((msg, index) => (
-      <p key={index}>{msg.data}</p>
-    ));
 
+  render() {
+    console.log(this.props.feed);
+    const messages = this.props.feed.map(msg => <Message message={msg.data} />);
+  
     return (
-      <div className="Feed">
-        <h2>Data Feed</h2>
-        {info}
+      <div className='Feed'>
+        <h2>Chat History</h2>
+        {messages}
       </div>
     );
-  }
+  };
 }
 
 export default Feed;
